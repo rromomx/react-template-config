9826cac406ba2c1daa0583a3f233cdf475b4dc30
Raul Romo <rromomxdev@gmail.com>
2020-04-27 19:53:10 -0500

    eslint fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import HelloWorld from './components/HelloWorld'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld';
 
 ReactDOM.render(
-  <HelloWorld />, 
-  document.getElementById('app')
-)
+  <HelloWorld />,
+  document.getElementById('app'),
+);
